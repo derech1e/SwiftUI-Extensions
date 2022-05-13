8c68c0ef9d164829ec396842f52aa21c37c100a9
derech1e <jojopaul88@gmail.com>
2022-05-13 22:55:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUI Extensions
+
+A description of this package.
---
+++ Sources/SwiftUI Extensions/SwiftUI_Extensions.swift
@@ -0,0 +1,6 @@
+public struct SwiftUI_Extensions {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
